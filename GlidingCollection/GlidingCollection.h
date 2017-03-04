a9e9b56eafc98c1c406d928846b3d1430fe1028e
Abdurahim Jauzee <jauzee@ya.ru>
2017-03-04 20:20:34 +0300

    created Xcode project

---
+++ GlidingCollection/GlidingCollection.h
@@ -0,0 +1,19 @@
+//
+//  GlidingCollection.h
+//  GlidingCollection
+//
+//  Created by Abdurahim Jauzee on 04/03/2017.
+//  Copyright © 2017 Ramotion Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GlidingCollection.
+FOUNDATION_EXPORT double GlidingCollectionVersionNumber;
+
+//! Project version string for GlidingCollection.
+FOUNDATION_EXPORT const unsigned char GlidingCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GlidingCollection/PublicHeader.h>
+
+
---
+++ GlidingCollectionDemo/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  GlidingCollectionDemo
+//
+//  Created by Abdurahim Jauzee on 04/03/2017.
+//  Copyright © 2017 Ramotion Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
