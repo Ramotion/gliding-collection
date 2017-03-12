f758e387e7694d11e321fbc7abf779a532ed8479
Abdurahim Jauzee <jauzee@ya.ru>
2017-03-13 02:20:47 +0300

    created GlidingCollection View

---
+++ GlidingCollection/GlidingCollection.swift
@@ -0,0 +1,13 @@
+//
+//  GlidingCollection.swift
+//  GlidingCollection
+//
+//  Created by Abdurahim Jauzee on 06/03/2017.
+//  Copyright © 2017 Ramotion Inc. All rights reserved.
+//
+
+import UIKit
+
+
+public class GlidingCollection: UIView {
+}
