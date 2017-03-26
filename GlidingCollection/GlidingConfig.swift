67303d578c387b38fc53a57cf41c0550b9d7279c
Abdurahim Jauzee <jauzee@ya.ru>
2017-03-26 13:37:35 +0300

    updated GlidingConfig
    - added shadow related properties

--- GlidingCollection/GlidingConfig.swift
+++ GlidingCollection/GlidingConfig.swift
@@ -17,7 +17,7 @@ public struct GlidingConfig {
   
   /// Side insets of GlidiingCollection view.
   /// Only left & right side insets will take effect.
-  public var sideInsets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30)
+  public var sideInsets = UIEdgeInsets(top: 10, left: 30, bottom: 10, right: 30)
   
   /// Duration of animation between GlidingCollection sections.
   public var animationDuration: Double = 0.3
@@ -26,7 +26,7 @@ public struct GlidingConfig {
   public var buttonsSpacing: CGFloat = 15
   
   /// Font of each element in vertical stack.
-  public var buttonsFont: UIFont = UIFont.systemFont(ofSize: 16)
+  public var buttonsFont = UIFont.systemFont(ofSize: 16)
   
   /// Scale factor of inactive sections buttons.
   public var buttonsScaleFactor: CGFloat = 0.65
@@ -41,6 +41,21 @@ public struct GlidingConfig {
   public var cardsSpacing: CGFloat = 30
   
   /// Size of collectionView's cells.
-  public var cardsSize: CGSize = CGSize(width: round(UIScreen.main.bounds.width * 0.65), height: round(UIScreen.main.bounds.height * 0.4))
+  public var cardsSize = CGSize(width: round(UIScreen.main.bounds.width * 0.65), height: round(UIScreen.main.bounds.height * 0.45))
+  
+  /// Apply parallax effect to horizontal cards.
+  public var isParallaxEnabled = true
+  
+  /// Shadow color.
+  public var cardShadowColor = UIColor.black
+  
+  /// Shadow offset: width - horizontal; height - vertical.
+  public var cardShadowOffset = CGSize(width: 0, height: 5)
+  
+  /// Shadow radius or blur.
+  public var cardShadowRadius: CGFloat = 7
+  
+  /// Shadow opacity.
+  public var cardShadowOpacity: Float = 0.3
 
 }
